8663e0841994668a29401c38a0af4ce9f723cf66
Jake G <jake098877@gmail.com>
2020-04-30 14:33:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     i = 0
     if name_hash.empty?
-      nil
+      return nil
     else
        arrv << value
        arrk << key
